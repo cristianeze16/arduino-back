5b7d5a773fdce7b3bdc84691603d781b82e6260e
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2023-05-05 22:05:51 -0300

    sin cors

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('./config/database');
-var cors = require('cors');
+// var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 
 var app = express();
 
@@ -22,8 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-
+// app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
